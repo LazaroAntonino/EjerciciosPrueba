9ad335dc0221457838ed94b928896753c71c4441
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-09-01 06:36:28 +0000

    Añadiendo ruta y componente para tutorial de UseEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-
 export const Home = () => {
-
-  const {store, dispatch} =useGlobalReducer()
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				Hola!
 			</p>
 		</div>
 	);
